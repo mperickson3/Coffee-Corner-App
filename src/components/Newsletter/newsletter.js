c04f90577531a1bfe55e077efd054144c2354a1c
mperickson3 <mperickson3@gmail.com>
2022-02-23 18:19:11 -0600

    Mostly display fixes

--- src/components/Newsletter/newsletter.js
+++ src/components/Newsletter/newsletter.js
@@ -5,9 +5,10 @@ import CoffeeCorners from "./coffeecorner.png";
 function Newsletter() {
   return (
     <div>
-      <div className="repeatCoffeeBox">
-        <img src={CoffeeCorners} alt="coffee corners"></img>
-        {/* coffee corner coffee corner coffee corner coffee corner coffee corner
+      <div className="column">
+        <div className="repeatCoffeeBox">
+          <img src={CoffeeCorners} alt="coffee corners"></img>
+          {/* coffee corner coffee corner coffee corner coffee corner coffee corner
         coff ffee corner coffee corner coffee corner coffee corner coffee corner
         coffee corner coffee ee corner coffee corner coffee corner coffee corner
         coffee corner coffee corner coffee co corner coffee corner coffee corner
@@ -16,22 +17,23 @@ function Newsletter() {
         coffee corner coffee corner coffee corner coffee corner coffee corner
         coffee corner r coffee corner coffee corner coffee corner coffee corner
         coffee corner coffee corner c coffee corne */}
-      </div>
-      <div className="newsletterBox">
-        <div className="newsLetterLogo">
-          <img src={logoSmall} alt="logoSmall"></img> newsletter
         </div>
-        <div className="newsLine" />
-        <div className="newsText">
-          {"Keep up-to-date with what's brewing at Coffee Corner."}
-        </div>
-        <div className="newsText">
-          Get a free coffee in-store when you sign up for our newsletter!
-        </div>
+        <div className="newsletterBox">
+          <div className="newsLetterLogo">
+            <img src={logoSmall} alt="logoSmall"></img> newsletter
+          </div>
+          <div className="newsLine" />
+          <div className="newsText">
+            {"Keep up-to-date with what's brewing at Coffee Corner."}
+          </div>
+          <div className="newsText">
+            Get a free coffee in-store when you sign up for our newsletter!
+          </div>
 
-        <input type="text" className="email"></input>
-        <button className="signUp">sign up</button>
-        <div className="unsubscribe">You can unsubscribe any time.</div>
+          <input type="text" className="email"></input>
+          <button className="signUp">sign up</button>
+          <div className="unsubscribe">You can unsubscribe any time.</div>
+        </div>
       </div>
     </div>
   );
